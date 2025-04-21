64c54ba0cc6fcd8092dcbb37534cba0875959284
tonightoo <tonightoo.development@gmail.com>
2025-04-22 00:08:45 +0900

    add shooting ability

--- src/states/playing.rs
+++ src/states/playing.rs
@@ -6,11 +6,16 @@ pub struct PlayingPlugin;
 #[derive(Component)]
 pub struct Player;
 
+#[derive(Component)]
+pub struct Bullet;
+
 impl Plugin for PlayingPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(GameState::Playing), setup_playing)
             .add_systems(Update, playing_system.run_if(in_state(GameState::Playing)))
             .add_systems(Update, player_movement.run_if(in_state(GameState::Playing)))
+            .add_systems(Update, player_shoot.run_if(in_state(GameState::Playing)))
+            .add_systems(Update, bullet_movement.run_if(in_state(GameState::Playing)))
             .add_systems(OnExit(GameState::Playing), cleanup_playing);
     }
 }
@@ -60,12 +65,53 @@ fn player_movement(
     }
 }
 
+fn player_shoot(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    query: Query<&Transform, With<Player>>,
+    mut commands: Commands,
+) {
+    if !keyboard.pressed(KeyCode::Space) {
+        return;
+    }
+
+    if let Ok(player_transform) = query.get_single() {
+        let player_pos = player_transform.translation;
+        commands.spawn((
+            Sprite {
+                color: Color::srgb(1.0, 1.0, 0.0),
+                custom_size: Some(Vec2::new(3.0, 3.0)),
+                ..default()
+            },
+            Transform {
+                translation: Vec3::new(player_pos.x, player_pos.y + 30.0, 0.0),
+                ..default()
+            },
+            Bullet,
+        ));
+    }
+}
+
+fn bullet_movement(
+    mut commands: Commands,
+    mut query: Query<(Entity, &mut Transform), With<Bullet>>,
+    time: Res<Time>,
+) {
+    const SPEED: f32 = 600.0;
+    for (entity, mut transform) in &mut query {
+        transform.translation.y += SPEED * time.delta_secs();
+
+        if transform.translation.y >= 360.0 {
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
 fn playing_system(
     mut next_state: ResMut<NextState<GameState>>,
     keyboard: Res<ButtonInput<KeyCode>>,
 ) {
     println!("Playing: Game is on!");
-    if keyboard.just_pressed(KeyCode::Space) {
+    if keyboard.just_pressed(KeyCode::Escape) {
         next_state.set(GameState::GameOver);
     }
 }
